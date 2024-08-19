18e069d2f969b54b76a9b58707d105783f71b60f
CeliaDiaz17 <Celia.diaz3@alu.uclm.es>
2024-08-19 13:49:35 +0200

    Lista de opciones con botones

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,47 @@
 #include <M5StickCPlus2.h>
 
+const char* opciones[] = {"Opcion A", "Opcion B", "Opcion C"};
+int numOpciones = 3;
+int opcionActual = 0;
+bool opcionSeleccionada = false;
+void mostrarOpciones();
+
 void setup() {
   M5.begin();
   M5.Lcd.setRotation(3);
-  M5.Lcd.setTextSize(3);
-  M5.Lcd.println("Holi! :3");
+  M5.Lcd.setTextSize(2);
+  mostrarOpciones();
 }
 
 void loop() {
+  M5.update();
+
+  if (!opcionSeleccionada) {
+    if (M5.BtnB.wasPressed()) {
+      opcionActual = (opcionActual + 1) % numOpciones;
+      mostrarOpciones();
+    }
+    
+    if (M5.BtnA.wasPressed()) {
+      opcionSeleccionada = true;
+      M5.Lcd.clear();
+      M5.Lcd.setCursor(0, 0);
+      M5.Lcd.printf("La opcion %c ha sido seleccionada", 'A' + opcionActual);
+    }
+  }
+
+  delay(100);
+}
+
+void mostrarOpciones() {
+  M5.Lcd.clear();
+  M5.Lcd.setCursor(0, 0);
+  for (int i = 0; i < numOpciones; i++) {
+    if (i == opcionActual) {
+      M5.Lcd.print("> ");
+    } else {
+      M5.Lcd.print("  ");
+    }
+    M5.Lcd.println(opciones[i]);
+  }
 }
